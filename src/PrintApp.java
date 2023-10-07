c1f1ee47b64bf37c460c05f02df9660c169b3fa6
mayuriyashodha <mayurifin@outlook.com>
2023-10-07 16:19:36 +0300

    Initial commit

---
+++ src/PrintApp.java
@@ -0,0 +1,41 @@
+
+public class PrintApp {
+
+	public static void main(String[] args) {
+		System.out.println("Printer App");
+		System.out.println("This App is made by");
+		 
+		 	String creator = "Mayuri yashodha";
+		 	System.out.println("This App is made by " + creator);
+	
+		 		int number1 = 10;
+		 		int number2 = 3;
+	
+		 		System.out.println(number1);
+		 		//print the value of number1 to the console.
+	
+	System.out.println("The value of number1 is " + number1);
+	
+	System.out.println("The value of number2 is " + number2);
+	
+	int Multiplication = (number1 * number2);	
+	System.out.println( Multiplication);
+	
+		int Addition = (number1 + number2);
+		System.out.println(Addition);
+	
+			int Substraction = (number1 - number2);
+			System.out.println(Substraction);
+	
+				double division = (number1 / number2);
+				System.out.println(division); 
+	
+	
+	
+		 
+		 
+		
+
+	}
+
+}
