f9bc824742f11de7726517bfde0bc2ccf8dbb825
mayuriyashodha <mayurifin@outlook.com>
2023-10-07 16:23:08 +0300

    Update PrintApp.java
    Changed the indentation.

--- src/PrintApp.java
+++ src/PrintApp.java
@@ -5,30 +5,30 @@ public class PrintApp {
 		System.out.println("Printer App");
 		System.out.println("This App is made by");
 		 
-		 	String creator = "Mayuri yashodha";
-		 	System.out.println("This App is made by " + creator);
+		String creator = "Mayuri yashodha";
+		System.out.println("This App is made by " + creator);
 	
-		 		int number1 = 10;
-		 		int number2 = 3;
+		int number1 = 10;
+		int number2 = 3;
 	
-		 		System.out.println(number1);
-		 		//print the value of number1 to the console.
+		System.out.println(number1);
+		//print the value of number1 to the console.
 	
-	System.out.println("The value of number1 is " + number1);
+		System.out.println("The value of number1 is " + number1);
 	
-	System.out.println("The value of number2 is " + number2);
+		System.out.println("The value of number2 is " + number2);
 	
-	int Multiplication = (number1 * number2);	
-	System.out.println( Multiplication);
+		int Multiplication = (number1 * number2);	
+		System.out.println( Multiplication);
 	
 		int Addition = (number1 + number2);
 		System.out.println(Addition);
 	
-			int Substraction = (number1 - number2);
-			System.out.println(Substraction);
+		int Substraction = (number1 - number2);
+		System.out.println(Substraction);
 	
-				double division = (number1 / number2);
-				System.out.println(division); 
+		double division = (number1 / number2);
+		System.out.println(division); 
 	
 	
 	
